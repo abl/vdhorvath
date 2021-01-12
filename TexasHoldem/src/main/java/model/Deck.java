c32e3f3d28e6f362b5a5adb7c243b4c8687440e6
Alexander Lash <167740+abl@users.noreply.github.com>
2021-01-11 20:19:42 -0800

    Rewrite Deck
    Deck is now strictly in terms of how everyone needs to access the deck and discard pile.
    
    Any operation that isn't needed by the game has been removed.
    
    This is, arguably, no longer a deck model - it's more of a controller - so it's a good candidate for more refactoring later on!

--- TexasHoldem/src/main/java/model/Deck.java
+++ TexasHoldem/src/main/java/model/Deck.java
@@ -1,182 +1,53 @@
-package Model;
+package model;
 
+import java.util.Stack;
+import java.util.Collections;
 
-import java.util.ArrayList;
-import java.util.EmptyStackException;
-import java.util.List;
-import java.util.Objects;
+public class Deck {
+  private Stack<Card> deck;
+  private Stack<Card> discards;
 
-
-
-/**
- * Model.DeckOfCards class implementing a Model.IStack Interface
- */
-
-public class DeckOfCards extends ADeckOfCards {
-  private List<Card> deck;
-  private Integer numberOfCardsInDeck = 52;
-
-
-  /**
-   * Model.DeckOfCards no argument Constructor to implement a Model.DeckOfCards.
-   */
-
-  public DeckOfCards() {
-    this.deck = new ArrayList<>();
-    this.populateDeck();
-
-  }
-
-
-  /**
-   * Getter for the deck
-   *
-   * @return a List of Cards
-   */
-
-
-  public List<Card> getDeck() {
-    return this.deck;
-  }
-
-
-  /**
-   * Setter for Model.DeckOfCards helper to shuffle cards
-   *
-   * @param index , Index would like to exchange within the deck.
-   * @param card  , Model.Card we are exchanging.
-   */
-
-  public void set(Integer index, Card card) {
-    this.deck.set(index, card);
-
-  }
-
-  /**
-   * Size of the Model.DeckOfCards
-   *
-   * @return number of cards in deck
-   */
-
-
-  public Integer size() {
-    return this.deck.size();
-  }
-
-
-  /**
-   * Tests if this Model.IStack is empty.
-   *
-   * @return true if and only if the Model.DeckOfCards contains no Cards. Otherwise return false.
-   */
-
-
-  @Override
-  public boolean isEmpty() {
-    return false;
-  }
-
-
-  /**
-   * Returns and removes the most recently-added item.
-   *
-   * @return The most recently-added item.
-   * @throws EmptyStackException Throws an EmptyStackException if the method is called on an empty
-   *                             Model.IStack. Note: EmptyStackException is a built-in Java
-   *                             exception.
-   */
-
-
-  @Override
-  public Card popCard() throws EmptyStackException {
-    return this.deck.remove(this.size() - 1);
-
-
-  }
-
-
-  /**
-   * Adds a Model.Card to the Model.DeckOfCards
-   *
-   * @param card A Model.Card push onto the Model.IStack.
-   */
-
-
-  @Override
-  public void addCard(Card card) {
-    this.deck.add(card);
-
-  }
-
-
-  /**
-   * Returns the most recently-added item.
-   *
-   * @return The most recently-added item. (Show)
-   * @throws EmptyStackException Throws an EmptyStackException if the method is called on an empty
-   *                             Model.IStack. Note: EmptyStackException is a built-in Java
-   *                             exception.
-   */
-
-
-  @Override
-  public Card showTopCard() throws EmptyStackException {
-    return this.deck.get(this.size() - 1);
-  }
-
-
-  /**
-   * Populate a deck of cards.
-   *
-   * @return A full deck of cards.
-   */
-
-  public void populateDeck() {
+  public Deck() {
+    deck = new Stack<>();
+    discards = new Stack<>();
+    
     for (Suit suit : Suit.values()) {
       for (Value value : Value.values()) {
-        Card newCard = new Card(suit, value);
-        addCard(newCard);
-
+        discards.push(new Card(suit, value));
       }
     }
-
+    shuffleDiscards();
   }
 
-
-  public Card getCard(Integer index) {
-    return this.deck.get(index);
+  public int getDeckSize() {
+    return deck.size();
   }
 
+  public int getDiscardSize() {
+    return discards.size();
+  }
 
-
-
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+  public Card drawCard() {
+    if (getDeckSize() == 0) {
+      shuffleDiscards();
     }
-    if (!(o instanceof DeckOfCards)) {
-      return false;
-    }
-    DeckOfCards that = (DeckOfCards) o;
-    return Objects.equals(deck, that.deck) &&
-        Objects.equals(numberOfCardsInDeck, that.numberOfCardsInDeck);
+
+    return deck.pop();
   }
 
-
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(deck, numberOfCardsInDeck);
+  public void discardCard(Card card) {
+    discards.push(card);
   }
 
-  @Override
-  public String toString() {
-    return "Model.DeckOfCards{" +
-        "deck=" + deck +
-        ", numberOfCardsInDeck=" + numberOfCardsInDeck +
-        '}';
+  public Card peekDiscard() {
+    // We might want this to show the top of the discard pile
+
+    return discards.peek();
+  }
+
+  private void shuffleDiscards() {
+    deck.addAll(discards);
+    Collections.shuffle(deck);
   }
 }
 
